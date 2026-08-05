82a9643a3f4a05166505aebf12f59ef6029d2fc7
Michael Stepniak <stepniak.michael@gmail.com>
2026-08-04 22:43:31 -0400

    Open .ttf cmap table -> convert keys to hex -> make a function to trim the input file name

--- unicode_glyph_reference/generate_reference.py
+++ unicode_glyph_reference/generate_reference.py
@@ -3,7 +3,11 @@ from fontTools import ttLib
 
 def main():
     file_name = input("File name: ")
-    print(file_name)
+
+    # Trim the file name to add to the .txt file
+    txt_output_name = trim_filename(file_name)
+
+    print(txt_output_name)
 
     if Path(file_name).is_file():
         print("The file exists")
@@ -12,16 +16,29 @@ def main():
     font = ttLib.TTFont(file_name)
 
     # Get the cmap table from the font.
+    # After testing, I know that:
+    # the value is a Python dict,
+    # and that the key value pairs are decimal_value: string_value
     font_cmap = font.getBestCmap()
     
-    # print(font_cmap.keys())
+    #convert keys in font_cmap dict to hex
+    hex_keys = []
+    for key in font_cmap:
+        hex_keys.append(hex(key))
+        
+    # print(hex_keys)
+ 
+def trim_filename(file_name):
+    output = []
+    for char in file_name:
+        if char == ".":
+            break
+        else:
+            output.append(char)
 
-    s = 0 
-
-    for k in font_cmap.keys():
-        s += 1
-
-    print(s)
+    return "".join(output)
+            
+        
 
 if __name__ == "__main__":
     main()
